c9f7128705843d8aafe419da9194b69e24839d7b
kani-kalimuthu <kani.cloudstor@gmail.com>
2023-04-07 19:12:03 +0530

    deploy

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,12 +5,12 @@
 
 # Step 1:
 # Create dockerpath
-DOCKERPATH=kanikalimuthu/image_capstone_kk
+DOCKERPATH=kanikalimuthu/image_capstone_kk_new
 
 # Step 2:  
 # Authenticate & tag
 docker login -u $DOCKER_USERNAME -p $DOCKER_PWD
-docker tag image_capstone_kk $DOCKERPATH
+docker tag image_capstone_kk_new $DOCKERPATH
 echo "Docker ID and Image: $DOCKERPATH"
 
 # Step 3:
